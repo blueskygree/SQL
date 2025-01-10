5c7d0f383c59e81437a940ca85780dd30926b94a
blueskygree <133464320+blueskygree@users.noreply.github.com>
2025-01-10 09:58:19 +0900

    Update 과일로_만든_아이스크림_구하기.sql

--- 과일로_만든_아이스크림_구하기.sql
+++ 과일로_만든_아이스크림_구하기.sql
@@ -1,1 +1,8 @@
-SELECT i.FLAVOR from FIRST_HALF as f JOIN ICECREAM_INFO as i on f.FLAVOR=i.FLAVOR where TOTAL_ORDER>3000 and INGREDIENT_TYPE='fruit_based' order by f.TOTAL_ORDER desc;
+SELECT i.FLAVOR
+from FIRST_HALF as f 
+JOIN ICECREAM_INFO as i on f.FLAVOR=i.FLAVOR 
+where TOTAL_ORDER>3000 and INGREDIENT_TYPE='fruit_based' 
+order by f.TOTAL_ORDER desc;
+
+
+#조인 확인할 것.
