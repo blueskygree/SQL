763f865ef7313a042f9ffaeebd1793b42ec1f0f1
blueskygree <133464320+blueskygree@users.noreply.github.com>
2025-01-20 15:59:25 +0900

    Create 오프라인_온라인_판매데이터_통합하기.sql

---
+++ 오프라인_온라인_판매데이터_통합하기.sql
@@ -0,0 +1,19 @@
+SELECT DATE_FORMAT(SALES_DATE,'%Y-%m-%d') as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+from (select SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+     from ONLINE_SALE
+     where SALES_DATE between '2022-03-01' and '2022-03-31'
+     
+     union all
+     
+     select SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT
+     from OFFLINE_SALE
+     where SALES_DATE between '2022-03-01' and '2022-03-31'
+     ) as combined_sales
+order by SALES_DATE, PRODUCT_ID, USER_ID;
+
+
+
+
+#union all 사용법 union all 사용시 별칭 꼭 삽입입
+#null 넣는것것
+#***** 매우 어렵다
