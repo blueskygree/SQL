39ae744d1e108be9cdcb298eca6861978d93da81
blueskygree <133464320+blueskygree@users.noreply.github.com>
2025-02-01 18:28:50 +0900

    Create 가격이_제일_비싼_식품의_정보_출력하기.sql

---
+++ 가격이_제일_비싼_식품의_정보_출력하기.sql
@@ -0,0 +1,13 @@
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+from FOOD_PRODUCT
+order by PRICE desc
+limit 1;
+
+
+or
+
+
+select PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+from FOOD_PRODUCT
+where PRICE = (select max(PRICE)
+              from FOOD_PRODUCT)
