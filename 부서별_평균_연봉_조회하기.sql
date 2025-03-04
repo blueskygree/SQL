27660f6506d24f95b7bb3810eabfc9b9dbbb6b3b
blueskygree <133464320+blueskygree@users.noreply.github.com>
2025-03-04 11:33:21 +0900

    Create 부서별_평균_연봉_조회하기.sql

---
+++ 부서별_평균_연봉_조회하기.sql
@@ -0,0 +1,6 @@
+select a.DEPT_ID, a.DEPT_NAME_EN, round(AVG(b.SAL),0) as AVG_SAL
+from HR_DEPARTMENT as a
+join HR_EMPLOYEES as b
+on a.DEPT_ID = b.DEPT_ID
+group by a.DEPT_ID
+order by AVG_SAL desc
