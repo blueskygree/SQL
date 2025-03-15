d461b74d25cf5d1e56251e931e81cfda6fa115f7
blueskygree <tjdwlsdn2020@naver.com>
2025-03-15 11:52:11 +0900

    Update 특정_물고기를_잡은_총_수_구하기.sql

--- 특정_물고기를_잡은_총_수_구하기.sql
+++ 특정_물고기를_잡은_총_수_구하기.sql
@@ -11,3 +11,10 @@ where FISH_TYPE in
 
 
 #살짝 어려웠다(하지만 혼자 품품)
+
+select count(*) as FISH_COUNT
+from FISH_INFO as a
+join FISH_NAME_INFO as b on a.FISH_TYPE=b.FISH_TYPE
+where b.FISH_NAME='BASS' or b.FISH_NAME='SNAPPER'
+
+# 쉬운 버전전
